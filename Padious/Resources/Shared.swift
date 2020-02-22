9c5161c9750a665995da6b2f4cbcd9134dd85b31
Tom Skypala <xskypala@gmail.com>
2020-02-22 22:12:21 +0100

    feat: add baseviewcontroller and basescreenview

---
+++ Padious/Resources/Shared.swift
@@ -0,0 +1,14 @@
+//
+//  Shared.swift
+//  Padious
+//
+//  Created by Tomas Skypala on 22/02/2020.
+//  Copyright © 2020 Tomas Skypala. All rights reserved.
+//
+
+import Foundation
+
+struct Shared {
+
+    static let appTheme = AppTheme()
+}
