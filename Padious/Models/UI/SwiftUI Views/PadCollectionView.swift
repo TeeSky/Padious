c1399307c83880ffd22e3ff570f0999bb07239f4
Tom Skypala <xskypala@gmail.com>
2020-03-07 02:02:12 +0100

    fix: make grid scaling usable

--- Padious/Models/UI/SwiftUI Views/PadCollectionView.swift
+++ Padious/Models/UI/SwiftUI Views/PadCollectionView.swift
@@ -24,31 +24,32 @@ struct PadCollectionView: View {
         self.padResourcesProvider = padResourcesProvider
         self.audioPlayer = audioPlayer
         self.gridMaker = gridMaker
+
+        UITableView.appearance().separatorStyle = .none
     }
 
     var body: some View {
         let padsGrid = gridMaker.makeGrid(from: padResourcesProvider.padResources)
 
         return ForEach(padsGrid) { gridRow in
-            HStack {
-                ForEach(gridRow.elements) { padResource in
-                    GeometryReader { geometry in
-
+            GeometryReader { geometry in
+                HStack {
+                    ForEach(gridRow.elements) { padResource in
                         Image(uiImage: UIImage(contentsOfFile: padResource.imageURL.path)!) // FIXME: get rid of the !
                             .resizable()
                             .scaledToFit()
                             .cornerRadius(Self.imageCornerRadius)
                             .frame(
-                                width: geometry.size.width,
-                                height: geometry.size.height,
+                                width: geometry.size.width / 3,
+                                height: geometry.size.width / 3,
                                 alignment: .center
                             )
                             .onTapGesture {
                                 self.audioPlayer.playSound(with: padResource.soundURL)
                             }
                     }
-                }.frame(minHeight: 125, maxHeight: .infinity)
-            }.padding(.vertical, 5)
+                }
+            }.frame(height: 125)
         }
     }
 }
