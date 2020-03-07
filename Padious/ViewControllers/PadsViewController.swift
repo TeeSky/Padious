49580d10e0b3e9771dfdb3ec202d4a2bec743d04
Tom Skypala <xskypala@gmail.com>
2020-03-08 00:28:34 +0100

    feat: enable pad sounds in silent mode
    Adds activation and deactivation of audio session in PadsViewController's viewDidAppear and viewWillDisappear functions.

--- Padious/ViewControllers/PadsViewController.swift
+++ Padious/ViewControllers/PadsViewController.swift
@@ -7,7 +7,20 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 final class PadsViewController: UIHostingController<PadsContentView> {
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        try? AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+        try? AVAudioSession.sharedInstance().setActive(true)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        try? AVAudioSession.sharedInstance().setActive(false)
+    }
 }
